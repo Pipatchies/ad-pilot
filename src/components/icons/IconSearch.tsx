73adaa93c811d3fc5011abe9d893e1875ce9b7d1
Pipatchies <ariane.bertaud@gmail.com>
2025-04-17 15:34:17 +0200

    🔧 build: convert SVG icons to React components

---
+++ src/components/icons/IconSearch.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgIconSearch = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={19.2} height={19.2} {...props}>
+    <path
+      fill="#3b2d57"
+      d="M15.24 13.476a8.13 8.13 0 0 0 1.68-5.009A8.424 8.424 0 0 0 8.52 0 8.526 8.526 0 0 0 0 8.467a8.526 8.526 0 0 0 8.52 8.467 8 8 0 0 0 5.04-1.67l3.6 3.578a1.165 1.165 0 0 0 1.68 0 1.15 1.15 0 0 0 0-1.67Zm-6.72.954A6.026 6.026 0 0 1 2.4 8.467a6.12 6.12 0 0 1 12.24 0 6.026 6.026 0 0 1-6.12 5.963"
+      data-name="icon_search"
+    />
+  </svg>
+);
+export default SvgIconSearch;
