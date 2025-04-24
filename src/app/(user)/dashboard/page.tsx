70fcd02c31ce4d45a5fe815e8f08d649972b73cc
Pipatchies <ariane.bertaud@gmail.com>
2025-04-24 15:50:09 +0200

    🚚 refactor: move LatestFiles to components

--- src/app/(user)/dashboard/page.tsx
+++ src/app/(user)/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import DetailsCard from "@/components/details-card";
 import Typography from "@/components/typography";
 import React from "react";
-import LatestFiles from "../campaign/components/latest-files";
+import LatestFiles from "../../../components/latest-files";
 
 const campaignData = [
   {
