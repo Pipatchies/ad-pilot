e13ba4a88e8b4b035db8258811bd90f8f88099df
Pipatchies <ariane.bertaud@gmail.com>
2025-04-24 18:06:07 +0200

    ✨ feat: initialize back button component

---
+++ src/app/(user)/targets/1/page.tsx
@@ -0,0 +1,12 @@
+import BackButton from '@/components/back-button'
+import React from 'react'
+
+
+
+export default function Target1() {
+  return (
+    <div>
+      <BackButton/>
+    </div>
+  )
+}
