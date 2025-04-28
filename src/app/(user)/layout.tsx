f0a7920773ac4f8422eaac9c4c02421584ba1927
Pipatchies <ariane.bertaud@gmail.com>
2025-04-28 09:23:47 +0200

    ✨ feat: add backButton component and put it in a new layout for specific pages like target or librairy

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,12 +1,18 @@
+"use client";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import MenuSidebar from "@/components/menu-sidebar";
 import Topbar from "@/components/topbar";
+import { usePathname } from "next/navigation";
+import BackButton from "@/components/back-button";
 
 export default function ClientLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const pathname = usePathname();
+    const showBackButton = /^\/(targets|librairy)\/[^\/]+$/.test(pathname);
   return (
     <>
       <SidebarProvider>
@@ -15,9 +21,16 @@ export default function ClientLayout({
           <header className="sticky top-0 z-50 border-b border-gray-200 sm:px-6 py-4 shadow-topbar bg-white ">
             <Topbar />
           </header>
-          <main className="py-10 px-5 @container max-w-7xl mx-auto w-full">
+          {showBackButton ? (
+          <main className="lg:py-20 lg:px-22 px-5 py-10 @container max-w-8xl mx-auto w-full">
+            <div className="flex flex-wrap items-start gap-10">
+              <BackButton />
             {children}
-          </main>
+            </div>
+            </main>
+            ) : (
+              <main className="py-20 p-5 @container max-w-7xl mx-auto w-full">{children}</main>
+          )}
           <footer className="mt-auto" />
         </div>
       </SidebarProvider>
--- src/app/(user)/targets/1/page.tsx
+++ src/app/(user)/targets/1/page.tsx
@@ -1,4 +1,4 @@
-import BackButton from '@/components/back-button'
+import Typography from '@/components/typography'
 import React from 'react'
 
 
@@ -6,7 +6,12 @@ import React from 'react'
 export default function Target1() {
   return (
     <div>
-      <BackButton/>
+        <Typography variant="h1">
+          Titre de la campagne
+        </Typography>
+        <Typography variant="h1" className="mb-10 font-bold">
+          Titre de la cible
+        </Typography>
     </div>
   )
 }
