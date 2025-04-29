d39dcdc0b7b196c5c4f123f5fc55484ee8b5106b
Pipatchies <ariane.bertaud@gmail.com>
2025-04-29 15:53:34 +0200

    ✨ feat: add possibility to show or unshow campaign colomn in Invoices Table

--- src/components/invoices-table.tsx
+++ src/components/invoices-table.tsx
@@ -14,7 +14,7 @@ type Invoice = {
   name: string;
   agencyInvoice?: string;
   regieName?: string;
-  campagne: string;
+  campagne?: string;
   htprice: number;
   ttcprice: number;
   date: Date;
@@ -24,56 +24,85 @@ type Invoice = {
 interface InvoiceTableProps {
   invoices: Invoice[];
   variant: "agency" | "regie";
+  showCampaign?: boolean;
 }
 
-export default function InvoicesTable({ invoices, variant }: InvoiceTableProps) {
-    return (
-      <div className="w-full overflow-x-auto">
-        <Table className="min-w-[700px]">
-          <TableHeader>
-            <TableRow className="border-none">
-              <TableHead className="w-[100px] text-base font-bold text-primary">N°de facture</TableHead>
+export default function InvoicesTable({
+  invoices,
+  variant,
+  showCampaign,
+}: InvoiceTableProps) {
+  return (
+    <div className="w-full overflow-x-auto">
+      <Table className="min-w-[700px]">
+        <TableHeader>
+          <TableRow className="border-none">
+            <TableHead className="w-[100px] text-base font-bold text-primary">
+              N°de facture
+            </TableHead>
+            {variant === "regie" && (
+              <TableHead className="text-base font-bold text-primary">
+                Facture agence rattachée
+              </TableHead>
+            )}
+            {variant === "regie" && (
+              <TableHead className="text-base font-bold text-primary px-10">
+                Régie
+              </TableHead>
+            )}
+            {showCampaign && (
+              <TableHead className="text-base font-bold text-primary">
+                Campagne
+              </TableHead>
+            )}
+            <TableHead className="text-base font-bold text-primary">
+              Montant HT
+            </TableHead>
+            <TableHead className="text-base font-bold text-primary px-5">
+              Montant TTC
+            </TableHead>
+            <TableHead className="text-base font-bold text-primary">
+              Date de facturation
+            </TableHead>
+            <TableHead className="text-base font-bold text-primary">
+              Date d'échéance
+            </TableHead>
+            <TableHead className="text-right" />
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {invoices.map((invoice, index) => (
+            <TableRow className="text-lg h-15 border-[#A5A4BF]" key={index}>
+              <TableCell className="pr-20">{invoice.name}</TableCell>
               {variant === "regie" && (
-                <TableHead className="text-base font-bold text-primary">Facture agence rattachée</TableHead>
-              )}
-              {variant === "regie" && (
-                <TableHead className="text-base font-bold text-primary px-10">Régie</TableHead>
-              )}
-              <TableHead className="text-base font-bold text-primary">Campagne</TableHead>
-              <TableHead className="text-base font-bold text-primary">Montant HT</TableHead>
-              <TableHead className="text-base font-bold text-primary px-5">Montant TTC</TableHead>
-              <TableHead className="text-base font-bold text-primary">Date de facturation</TableHead>
-              <TableHead className="text-base font-bold text-primary">Date d'échéance</TableHead>
-              <TableHead className="text-right" />
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {invoices.map((invoice, index) => (
-              <TableRow className="text-lg h-15" key={index}>
-                <TableCell className="pr-10">{invoice.name}</TableCell>
-                {variant === "regie" && (
                 <TableCell>{invoice.agencyInvoice}</TableCell>
-                )}
-                {variant === "regie" && (
-                <TableCell className="px-10 font-bold underline">{invoice.regieName}</TableCell>
-                )}
-                <TableCell className="font-bold underline pr-10">{invoice.campagne}</TableCell>
-                <TableCell>{invoice.htprice.toLocaleString("fr-FR")} €</TableCell>
-                <TableCell className="px-5">{invoice.ttcprice.toLocaleString("fr-FR")} €</TableCell>
-                <TableCell>{invoice.date.toLocaleDateString()}</TableCell>
-                <TableCell>{invoice.dueDate.toLocaleDateString()}</TableCell>
-                <TableCell className="text-right">
-                  <div className="flex justify-end gap-4">
-                    <SvgEyeIcon />
-                    <SvgUploder />
-                  </div>
+              )}
+              {variant === "regie" && (
+                <TableCell className="px-10 font-bold underline">
+                  {invoice.regieName}
                 </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
-    );
-  }
-  
-
+              )}
+              {showCampaign && (
+                <TableCell className="font-bold underline pr-10">
+                  {invoice.campagne}
+                </TableCell>
+              )}
+              <TableCell>{invoice.htprice.toLocaleString("fr-FR")} €</TableCell>
+              <TableCell className="px-5">
+                {invoice.ttcprice.toLocaleString("fr-FR")} €
+              </TableCell>
+              <TableCell>{invoice.date.toLocaleDateString()}</TableCell>
+              <TableCell>{invoice.dueDate.toLocaleDateString()}</TableCell>
+              <TableCell className="text-right">
+                <div className="flex justify-end gap-4">
+                  <SvgEyeIcon />
+                  <SvgUploder />
+                </div>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
