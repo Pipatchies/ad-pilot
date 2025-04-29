7b639c2bef32dfe4e5b51363b87845ff2a676548
Pipatchies <ariane.bertaud@gmail.com>
2025-04-29 11:37:46 +0200

    ✏️ refacto: remove unnecessary css

--- src/components/icons/Crayon.tsx
+++ src/components/icons/Crayon.tsx
@@ -5,10 +5,11 @@ const SvgCrayon = (props: SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     width={13.062}
     height={13.062}
+    viewBox="0 0 13.062 13.062"
+    fill="currentColor"
     {...props}
   >
     <path
-      fill="#3b2d57"
       d="M6.657 2.512 2.01 7.285 0 13.062l5.777-1.884 4.647-4.647Zm5.9-.377L10.927.5a1.523 1.523 0 0 0-2.261 0L7.285 1.884l3.768 3.893L12.56 4.27a1.6 1.6 0 0 0 .5-1.13 1.55 1.55 0 0 0-.5-1.005Z"
       data-name="Trac\xE9 721"
     />
