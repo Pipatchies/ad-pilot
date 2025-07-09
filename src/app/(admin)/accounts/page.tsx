e7647c848b056317fcdb2dadd5e79023e4f62e36
Pipatchies <ariane.bertaud@gmail.com>
2025-07-09 11:48:16 +0200

    ✨ feat: add tittle and CTA button to create a new account in account page

--- src/app/(admin)/accounts/page.tsx
+++ src/app/(admin)/accounts/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import CtaButton from "@/components/cta-button";
+import SvgPlus from "@/components/icons/Plus";
+import Typography from "@/components/typography";
+import React from "react";
+
+const ctaProps = { text: "Ajouter un compte", url: "#", target: "self" };
 
 export default function page() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="flex flex-col gap-10">
+      <div className="flex flex-wrap items-center justify-between">
+        <Typography variant="h1">Comptes</Typography>
+        <CtaButton
+          props={ctaProps}
+          icon={<SvgPlus />}
+          className="mr-0"
+          variant="openModal"
+        />
+      </div>
+    </section>
+  );
 }
