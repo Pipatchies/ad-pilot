e42a33afaabc864d171208d3c76b7c8eb3c2d9e4
Pipatchies <ariane.bertaud@gmail.com>
2025-07-09 18:18:23 +0200

    ✨ feat: create Modal component and integrate it in Accounts page to add an admin

--- src/app/(admin)/accounts/page.tsx
+++ src/app/(admin)/accounts/page.tsx
@@ -1,23 +1,16 @@
-import CtaButton from "@/components/cta-button";
-import SvgPlus from "@/components/icons/Plus";
 import SearchBar from "@/components/search-bar";
 import Typography from "@/components/typography";
 import React from "react";
+import AccountModal from "./_sections/accountModal";
 
-const ctaProps = { text: "Ajouter un compte", url: "#", target: "self" };
 
 export default function Accounts() {
   return (
     <section className="flex flex-col gap-2">
       <div className="flex flex-wrap items-center justify-between">
         <Typography variant="h1" className="mb-0">Comptes</Typography>
-        <CtaButton
-          props={ctaProps}
-          icon={<SvgPlus />}
-          className="mr-0"
-          variant="openModal"
-        />
-      </div>
+        <AccountModal />
+        </div>
       <p>6 comptes</p>
       <div className="flex justify-end">
       <SearchBar variant="minimal" />
---
+++ src/components/modal.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { ReactNode } from "react";
+import { Button } from "./ui/button";
+
+type ModalProps = {
+  cta: {
+    text: string;
+    icon: React.ReactNode;
+  };
+  data: {
+    title?: string;
+    description?: string;
+    children: ReactNode;
+    footer?: ReactNode;
+    subject?: string;
+  };
+};
+
+export default function Modal({ cta, data }: ModalProps) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          variant="ghost"
+          className=
+            "group flex items-center gap-2 p-5 rounded-sm text-base font-semibold transition border border-primary text-primary hover:bg-primary hover:text-white"
+        >
+          <div className="mr-2">{cta.icon}</div>
+            <span className="block !leading-none relative font-[600] cursor-pointer">
+              {cta.text}
+            </span>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="w-full max-w-2xl">
+        <DialogHeader>
+          <DialogTitle>{data.title}</DialogTitle>
+          <DialogDescription>{data.description}</DialogDescription>
+        </DialogHeader>
+        <div className="mt-4">{data.children}</div>
+        <DialogFooter className="mt-6">
+            <DialogClose asChild>
+              {data.footer}
+            </DialogClose>
+          </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
