755afc7880ac9b67d5ae567d0602b36200d2f84d
Pipatchies <ariane.bertaud@gmail.com>
2025-07-09 10:35:52 +0200

    ✨ feat: create dashboard page (no data yet)

---
+++ src/app/(admin)/adminDashboard/page.tsx
@@ -0,0 +1,99 @@
+import LatestFiles from '@/components/latest-files'
+import Typography from '@/components/typography'
+import React from 'react'
+
+const campaignData = [
+  {
+  title: "Titre lorem ipsum",
+  description: "Nom du client",
+  startDate: new Date("2025-01-13"),
+  endDate: new Date("2025-03-30"),
+  status: "Brief",
+  icons: [ {
+    name: "panneau",
+    url: "/icons/panneau-daffichage.svg",
+    width: 15,
+    height: 26,
+  },
+  {
+    name: "radio",
+    url: "/icons/radio.svg",
+      width: 28,
+      height: 27,
+  },
+  {
+    name: "television",
+    url: "/icons/television.svg",
+      width: 26,
+      height: 20,
+  },
+],
+},
+{
+  title: "Titre lorem ipsum",
+  description: "Nom du client",
+  startDate: new Date("2025-01-13"),
+  endDate: new Date("2025-03-30"),
+  status: "Brief",
+  icons: [ {
+    name: "panneau",
+    url: "/icons/panneau-daffichage.svg",
+    width: 15,
+    height: 26,
+  },
+  {
+    name: "radio",
+    url: "/icons/radio.svg",
+      width: 28,
+      height: 27,
+  },
+  {
+    name: "television",
+    url: "/icons/television.svg",
+      width: 26,
+      height: 20,
+  },
+],
+},
+{
+  title: "Titre lorem ipsum",
+  description: "Nom du client",
+  startDate: new Date("2025-01-13"),
+  endDate: new Date("2025-03-30"),
+  status: "Brief",
+  icons: [ {
+    name: "panneau",
+    url: "/icons/panneau-daffichage.svg",
+    width: 15,
+    height: 26,
+  },
+  {
+    name: "radio",
+    url: "/icons/radio.svg",
+      width: 28,
+      height: 27,
+  },
+  {
+    name: "television",
+    url: "/icons/television.svg",
+      width: 26,
+      height: 20,
+  },
+],
+},
+]
+
+export default function Dashboard() {
+  return (
+    <section className="flex flex-col gap-10">
+      <Typography variant="h1">
+        Tableau de bord
+      </Typography>
+      <LatestFiles
+        title="Campagnes en cours"
+        data={campaignData}
+        variant="campaign"
+      />
+    </section>
+  )
+}
