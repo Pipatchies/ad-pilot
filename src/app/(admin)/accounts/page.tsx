c27a63d341e66885c46b8e71a1f902f388d6e78c
Pipatchies <ariane.bertaud@gmail.com>
2025-07-09 16:01:59 +0200

    ✨ feat: add searchbar in accounts page

--- src/app/(admin)/accounts/page.tsx
+++ src/app/(admin)/accounts/page.tsx
@@ -1,21 +1,26 @@
 import CtaButton from "@/components/cta-button";
 import SvgPlus from "@/components/icons/Plus";
+import SearchBar from "@/components/search-bar";
 import Typography from "@/components/typography";
 import React from "react";
 
 const ctaProps = { text: "Ajouter un compte", url: "#", target: "self" };
 
-export default function page() {
+export default function Accounts() {
   return (
-    <section className="flex flex-col gap-10">
+    <section className="flex flex-col gap-2">
       <div className="flex flex-wrap items-center justify-between">
-        <Typography variant="h1">Comptes</Typography>
+        <Typography variant="h1" className="mb-0">Comptes</Typography>
         <CtaButton
           props={ctaProps}
           icon={<SvgPlus />}
           className="mr-0"
           variant="openModal"
         />
+      </div>
+      <p>6 comptes</p>
+      <div className="flex justify-end">
+      <SearchBar variant="minimal" />
       </div>
     </section>
   );
