f431bdbb3c42247d6c1df74f7c269e2abbc6d120
Pipatchies <ariane.bertaud@gmail.com>
2025-12-05 15:01:58 +0100

    feat: add campaigns page with sections

---
+++ src/app/admin/campaigns/_sections/spaceCampaignsHeader.tsx
@@ -0,0 +1,37 @@
+import Typography from "@/components/typography";
+import React from "react";
+import SearchBar from "@/components/search-bar";
+import { useQuery } from "convex/react";
+import { api } from "../../../../../convex/_generated/api";
+import CtaButton from "@/components/cta-button";
+import SvgCrayonBig from "@/components/icons/CrayonBig";
+
+const CtaProps = {
+  text: "Ajouter un document",
+  icon: <SvgCrayonBig />,
+};
+
+type campaignsHeaderProps = {
+  onQueryChange: (q: string) => void;
+};
+
+export default function SpaceCampaignsHeader({
+  onQueryChange,
+}: campaignsHeaderProps) {
+  const campaigns = useQuery(api.queries.campaigns.getAllCampaigns) ?? [];
+  const count = campaigns.length;
+  return (
+    <section>
+      <div className="flex flex-wrap items-center justify-between">
+        <Typography variant="h1" className="mb-0">
+          Campagnes
+        </Typography>
+        <CtaButton props={CtaProps} />
+      </div>
+      <p className="text-primary/80">{`${count} campagnes`}</p>
+      <div className="flex justify-end">
+        <SearchBar variant="minimal" onQueryChange={onQueryChange} />
+      </div>
+    </section>
+  );
+}
---
+++ src/app/admin/campaigns/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SpaceCampaigns from "./_sections/spaceCampaigns";
+
+export default function CampaignsPage() {
+  return (
+  <section>
+    <SpaceCampaigns />
+  </section>
+  )
+}
+
