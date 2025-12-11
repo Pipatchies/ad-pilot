fe6ccf20b56744b44c14f273129136722337c042
Pipatchies <ariane.bertaud@gmail.com>
2025-12-11 14:58:00 +0100

    feat: restructure update campaign page and add dynamic campaign title

--- src/app/admin/campaigns/[id]/page.tsx
+++ src/app/admin/campaigns/[id]/page.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import CampaignForm from "../../new-campaign/components/campaign-form";
 import { Id } from "../../../../../convex/_generated/dataModel";
-import Typography from "@/components/typography";
-import CtaButton from "@/components/cta-button";
+import SpaceCampaignHeader from "./_sections/spaceCampaignHeader";
 
 const ctaProps = {
   text: "Enregistrer les modifications",
@@ -13,17 +12,8 @@ const ctaProps = {
 export default function EditCampaignPage({ params }: { params: { id: Id<"campaigns"> } }) {
   return (
   <section className="space-y-6">
-        <div className='flex flex-wrap items-center justify-between'>
-        <div className="flex flex-col">
-          <Typography variant="h1">Campagne</Typography>
-          <Typography variant="h2">Titre de la campagne</Typography>
-        </div>
-        <CtaButton
-            props={ctaProps}
-            variant="submit"
-              />
-        </div>
-        <CampaignForm campaignId={params.id} />
+    <SpaceCampaignHeader campaignId={params.id} />
+    <CampaignForm campaignId={params.id} />
     </section>
   )
 }
