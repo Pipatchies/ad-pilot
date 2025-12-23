45ec2727c0237a6d4ace46b639982524ae8f1a84
Pipatchies <ariane.bertaud@gmail.com>
2025-12-23 18:33:34 +0100

    refactor: restructurize organization details page

--- src/app/admin/clients/[id]/page.tsx
+++ src/app/admin/clients/[id]/page.tsx
@@ -4,6 +4,11 @@ import { useQuery } from "convex/react";
 import { api } from "@/../convex/_generated/api";
 import { Id } from "@/../convex/_generated/dataModel";
 import { useParams } from "next/navigation";
+import UserCard from "./components/user-card";
+import SpaceCampaignsActiveTable from "./_sections/spaceCampaignsActiveTable";
+import SpaceCampaignsArchivedTable from "./_sections/spaceCampaignsArchivedTable";
+import SpaceOrgnizationDetailsHeader from "./_sections/spaceOrgnizationDetailsHeader";
+
 
 export default function ClientDetailsPage() {
   const { id } = useParams();
@@ -15,39 +20,20 @@ export default function ClientDetailsPage() {
 
   const { organization, user, campaigns } = data;
 
-  const activeCampaigns = campaigns.filter((c) => {
-    const endDate = new Date(c.endDate);
-    const now = new Date();
-    return endDate >= now;
-  });
-
-  const archivedCampaigns = campaigns.filter((c) => {
-    const endDate = new Date(c.endDate);
-    const now = new Date();
-    return endDate < now;
-  });
-
   return (
-    <div className="p-10 space-y-10">
-      <h1 className="text-4xl font-bold">{organization.name}</h1>
-
+   <section className="p-10">
+      <SpaceOrgnizationDetailsHeader organization={organization} />
       {/* Card Info User */}
-      <section className="border p-4 rounded-md">
-        <h2 className="text-xl font-bold mb-2">Info User</h2>
-        <pre>{JSON.stringify(user, null, 2)}</pre>
-      </section>
+      <div className="space-y-8">
+        <UserCard user={user} />
+      
 
       {/* Table Campagnes en cours */}
-      <section className="border p-4 rounded-md">
-        <h2 className="text-xl font-bold mb-2">Campagnes en cours</h2>
-        <p>Table ici ({activeCampaigns.length})</p>
-      </section>
+      <SpaceCampaignsActiveTable campaigns={campaigns} />
 
       {/* Table Campagnes archivées */}
-      <section className="border p-4 rounded-md">
-        <h2 className="text-xl font-bold mb-2">Campagnes archivées</h2>
-        <p>Table ici ({archivedCampaigns.length})</p>
-      </section>
-    </div>
+      <SpaceCampaignsArchivedTable campaigns={campaigns} />
+      </div>
+    </section>
   );
 }
