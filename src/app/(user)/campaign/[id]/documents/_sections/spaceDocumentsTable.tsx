1edcffe526e2666fffe5300a56bbb2e8b9e05d2f
Pipatchies <ariane.bertaud@gmail.com>
2025-12-30 15:46:54 +0100

    feat: display documents datas with query in documents page on client

--- src/app/(user)/campaign/[id]/documents/_sections/spaceDocumentsTable.tsx
+++ src/app/(user)/campaign/[id]/documents/_sections/spaceDocumentsTable.tsx
@@ -1,63 +1,11 @@
 "use client";
-import DocumentsTable from "../../../../../../components/table/documents-table";
+import React from "react";
+import DocumentsTable from "@/components/table/documents-table";
+import { useQuery } from "convex/react";
+import { Id } from "../../../../../../../convex/_generated/dataModel";
+import { api } from "../../../../../../../convex/_generated/api";
+import { useParams } from "next/navigation";
 
-const documentsData = [
-  {
-    title: "Justificatif de diffusion",
-    type: "PDF",
-    date: new Date("2025-01-13").getTime(),
-  },
-  {
-    title: "Justificatif de diffusion",
-    type: "PDF",
-    date: new Date("2025-01-13").getTime(),
-  },
-  {
-    title: "Justificatif de diffusion",
-    type: "PDF",
-    date: new Date("2025-01-13").getTime(),
-  },
-  {
-    title: "Justificatif de diffusion",
-    type: "PDF",
-    date: new Date("2025-01-13").getTime(),
-  },
-  {
-    title: "Justificatif de diffusion",
-    type: "PDF",
-    date: new Date("2025-01-13").getTime(),
-  },
-  {
-    title: "Justificatif de diffusion",
-    type: "PDF",
-    date: new Date("2025-01-13").getTime(),
-  },
-  {
-    title: "Justificatif de diffusion",
-    type: "PDF",
-    date: new Date("2025-01-13").getTime(),
-  },
-  {
-    title: "Justificatif de diffusion",
-    type: "PDF",
-    date: new Date("2025-01-13").getTime(),
-  },
-  {
-    title: "Justificatif de diffusion",
-    type: "PDF",
-    date: new Date("2025-01-13").getTime(),
-  },
-  {
-    title: "Justificatif de diffusion",
-    type: "PDF",
-    date: new Date("2025-01-13").getTime(),
-  },
-  {
-    title: "Justificatif de diffusion",
-    type: "PDF",
-    date: new Date("2025-01-13").getTime(),
-  },
-];
 
 type DocumentsTableProps = {
   globalFilter: string;
@@ -68,13 +16,27 @@ export default function SpaceDocumentsTable({
   globalFilter,
   dateSort,
 }: DocumentsTableProps) {
+  
+    const params = useParams();
+    const campaignId = params?.id as Id<"campaigns">;
+  
+    const documents = useQuery(api.queries.documents.getDocumentsByCampaign, {
+      campaignId,
+    });
+
+  const documentsData = documents?.map((doc) => ({
+    ...doc,
+  })) ?? [];
+
   return (
     <section>
       <div className="overflow-x-auto">
         <DocumentsTable
           documents={documentsData}
           globalFilter={globalFilter}
-          dateSort={dateSort}
+          showCampaign={false}
+          showClient={false}
+          readOnly={true}
         />
       </div>
     </section>
